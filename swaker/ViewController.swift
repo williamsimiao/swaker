b92340ea43d1e1599b34e284cff980b049149f8b
andreanmasiro <andreanmasiro@live.com>
2015-07-24 15:05:05 -0300

    Frameworks & initial settings

--- swaker/ViewController.swift
+++ swaker/ViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let user = PFUser.logInWithUsername("andreanmasiro@live.com", password: "1234")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
