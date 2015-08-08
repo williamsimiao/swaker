bda7b3fc8fd818ab99a507c320d3c87636d41a09
andreanmasiro <andreanmasiro@live.com>
2015-08-08 19:14:14 -0300

    Just a color

--- swaker/Colors.swift
+++ swaker/Colors.swift
@@ -13,8 +13,9 @@ let darkBlue = UIColor(red: 0, green: 71/255, blue: 129/255, alpha: 1.0)
 let lightBlue = UIColor(red: 194/255, green: 236/255, blue: 238/255, alpha: 1.0)
 let yellow = UIColor(red: 255/255, green: 175/255, blue: 0/255, alpha: 1.0)
 let selectedTintColor = UIColor(red: 255/255, green: 159/255, blue: 0/255, alpha: 1.0)
-let separatorColor = UIColor(red: 199.9/255, green: 199/255, blue: 204/255, alpha: 1.0)
+//let separatorColor = UIColor(red: 199.9/255, green: 199/255, blue: 204/255, alpha: 1.0)
 let navBarTintColor = UIColor(red: 2/255, green: 106/255, blue: 190/255, alpha: 1.0)
+let separatorColor = navBarTintColor
 
 
 let mainColors = [darkBlue.CGColor, lightBlue.CGColor, yellow.CGColor]
