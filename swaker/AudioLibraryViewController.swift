c3becad8363560d5683450777f1711378e3a02bc
williamsimiao <williamsimiao@gmail.com>
2015-08-11 14:12:18 -0300

    uehueheuhruehruehruehr

--- swaker/AudioLibraryViewController.swift
+++ swaker/AudioLibraryViewController.swift
@@ -76,6 +76,12 @@ class AudioLibraryViewController: UIViewController, UITableViewDataSource, UITab
     
     
     @IBAction func play(sender:AnyObject) {
+        if playingCell != nil {
+            playingCell!.switchToPlay()
+            audioPlayer.pause()
+            playingCell = nil
+            audioPlayer = nil
+        }
         let sv = sender.superview!
         let cell = sv!.superview as! AudioCell
         playingCell = cell
@@ -97,6 +103,7 @@ class AudioLibraryViewController: UIViewController, UITableViewDataSource, UITab
     
     func audioPlayerDidFinishPlaying(player: AVAudioPlayer!, successfully flag: Bool) {
         playingCell?.switchToPlay()
+        playingCell = nil
     }
     
     // MARK: - Table view data source
